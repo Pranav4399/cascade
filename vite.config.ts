2bc4e03ada849ad1d395650a3adcb894b7d43a18
Akaash Preetham <58697757+akaashpreetham1@users.noreply.github.com>
2025-07-02 15:19:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: '/cascade/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
